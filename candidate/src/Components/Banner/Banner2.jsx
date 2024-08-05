94ce26153c1b08157f0b514b152b6e0c7e7078cb
Hrishabh Tiwari <rishabh2560@gmail.com>
2024-08-05 19:10:06 +0530

    hero section update

--- candidate/src/Components/Banner/Banner2.jsx
+++ candidate/src/Components/Banner/Banner2.jsx
@@ -1,28 +1,45 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Banner2 = () => {
   return (
     <section className="jm-banner-area-2 banner-hieght bg-default jm-overlay h-jobmot-banner-2">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="jm-banner-content-static">
-                        <div className="jm-banner-content-haed text-center">
-                            <h2 className="jm-banner-content-title-3">
-                                <span className="jm-banner-bold-amount">Finding your</span> <br /><span className='text-[#F59300]'> Perfect career path</span> <br /> <span className="jm-banner-bold-amount">starts here</span>
-                            </h2>
-                            <span className="jm-banner-sub-text">Find Jobs, Employment & Career Opportunities</span>
-                            <p>There are many variations of passages of Lorem Ipsum Fasts <br/>
-                                by injected humour, or randomised words which... </p>
-                            <Link to="/jobListPage" className="jm-banner-layout-2-btn mt-25">Get Started <i className="fa-thin fa-arrow-right-long"></i></Link>
-                        </div>
-                    </div>
-                </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="jm-banner-content-static">
+              <div className="jm-banner-content-haed text-center">
+                <h2 className="jm-banner-content-title-3">
+                  <span className="jm-banner-bold-amoun">Finding your</span>{" "}
+                  <br />
+                  <span className="text-[#F59300]">
+                    {" "}
+                    Perfect career path
+                  </span>{" "}
+                  <br />{" "}
+                  <span className="jm-banner-bold-amout">starts here</span>
+                </h2>
+                <span className="jm-banner-sub-text">
+                  Find Jobs, Employment & Career Opportunities
+                </span>
+                <p>
+                  There are many variations of passages of Lorem Ipsum Fasts{" "}
+                  <br />
+                  by injected humour, or randomised words which...{" "}
+                </p>
+                <Link
+                  to="/jobListPage"
+                  className="jm-banner-layout-2-btn mt-25"
+                >
+                  Get Started <i className="fa-thin fa-arrow-right-long"></i>
+                </Link>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Banner2
+export default Banner2;
