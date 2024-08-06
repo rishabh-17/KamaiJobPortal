ef1c93be596acdabe47bbde2f1602e4eed4b6474
Hrishabh Tiwari <rishabh2560@gmail.com>
2024-08-06 21:46:57 +0530

    register

--- candidate/src/Components/Video Area/VideoArea2.jsx
+++ candidate/src/Components/Video Area/VideoArea2.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import VideoTab from './VideoTab'
-import Counter from './Counter'
-import VideoModalArea from './VideoModalArea'
+import React from "react";
+import VideoTab from "./VideoTab";
+import Counter from "./Counter";
+import VideoModalArea from "./VideoModalArea";
 
 const VideoArea2 = () => {
   return (
     <div className="jm-video-area-3 jm-video-area-layout-3">
-        <div className="jm-video-img-3 bg-default">
-            <VideoTab/>
-            <VideoModalArea/>
-        </div>
-        <Counter/>
+      <div className="jm-video-img-3 bg-default">
+        <VideoTab />
+        <VideoModalArea />
+      </div>
+      <Counter />
     </div>
-  )
-}
+  );
+};
 
-export default VideoArea2
+export default VideoArea2;
