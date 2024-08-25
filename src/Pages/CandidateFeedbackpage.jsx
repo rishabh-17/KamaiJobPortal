3fc41975622b2a31e05709a3e46f40ff860718a1
Hrishabh Tiwari <rishabh2560@gmail.com>
2024-08-25 23:04:24 +0530

    spell fix

--- src/Pages/CandidateFeedbackpage.jsx
+++ src/Pages/CandidateFeedbackpage.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
-import CandidateFeedbackMain from "../Components/Main/CandidateFeedbackMain";
+import CandidateFeedbackMain from "../Components/Main/CandidateFeedbackmain";
 
 const CandidateFeedbackPage = () => {
   return (
